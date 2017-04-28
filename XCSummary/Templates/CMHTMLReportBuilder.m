eabd2d958f1dd4cb59865229950630f952ab81fd
a.y.ivanov2 <a.y.ivanov2@tinkoff.ru>
2017-04-28 14:44:30 +0300

    убраны лишние скрины добавлен тест assert fail.

--- XCSummary/Templates/CMHTMLReportBuilder.m
+++ XCSummary/Templates/CMHTMLReportBuilder.m
@@ -64,7 +64,6 @@
 {
     NSUInteger successfullTests = [[summaries valueForKeyPath:@"@sum.numberOfSuccessfulTests"] integerValue];
     NSUInteger failedTests = [[summaries valueForKeyPath:@"@sum.numberOfFailedTests"] integerValue];
- 
     BOOL failuresPresent = failedTests > 0;
     NSString *templateFormat = [self _decodeTemplateWithName:SummaryTemplate];
     NSTimeInterval totalTime = [[summaries valueForKeyPath:@"@sum.totalDuration"] doubleValue];
@@ -154,12 +153,19 @@
         NSString *localImageName = [NSString stringWithFormat:@"resources/Screenshot_%@.png", activity.uuid.UUIDString];
         composedString = [NSString stringWithFormat:templateFormat, activity.title, activity.finishTimeInterval - activity.startTimeInterval, localImageName, localImageName];
         
-        [self.resultString appendString:composedString];
+        // Убираем лишние скрины
+        if (![activity.title isEqualToString:@"Wait for app to idle"]) {
+            [self.resultString appendString:composedString];
+        }
     }
     else
     {
-//        templateFormat = [self _decodeTemplateWithName:ActivityTemplateWithoutImage];
-//        composedString = [NSString stringWithFormat:templateFormat, indentation, @"px", activity.title, activity.finishTimeInterval - activity.startTimeInterval];
+        // Добавляем error message
+        if ([activity.title containsString:@"Assertion Failure"]) {
+            templateFormat = [self _decodeTemplateWithName:ActivityTemplateWithoutImage];
+            composedString = [NSString stringWithFormat:templateFormat, indentation, @"px", activity.title, activity.finishTimeInterval - activity.startTimeInterval];
+            [self.resultString appendString:composedString];
+        }
     }
 }
 
